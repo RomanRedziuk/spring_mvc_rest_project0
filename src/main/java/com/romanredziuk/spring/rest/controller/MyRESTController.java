fb33104d65b7a1ad4af286795b7f7243a7e230ac
Roman Redziuk <r8mano@gmail.com>
2022-10-25 17:38:58 +0100

    Bugs fixed 1.7

--- src/main/java/com/romanredziuk/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/romanredziuk/spring/rest/controller/MyRESTController.java
@@ -54,14 +54,14 @@ public class MyRESTController {
     }
 
     @DeleteMapping("/employees/{id}")
-    public String deleteEmployee(@PathVariable int id){
+    public String deleteEmployee(@PathVariable int id) {
 
         Employee employee = employeeService.getEmployee(id);
-        if(employee==null){
-            throw new NoSuchEmployeeException("There is no employee with ID= " + id+ " in Database!");
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID= " + id + " in Database!");
         }
 
         employeeService.deleteEmployee(id);
-        return "Employee with ID = "+id+" was deleted.";
+        return "Employee with ID = " + id + " was deleted.";
     }
 }
--- src/main/java/com/romanredziuk/spring/rest/entity/Employee.java
+++ src/main/java/com/romanredziuk/spring/rest/entity/Employee.java
@@ -20,12 +20,12 @@ public class Employee {
     private String department;
 
     @Column(name = "salary")
-    private String salary;
+    private int salary;
 
     public Employee() {
     }
 
-    public Employee(String name, String surname, String department, String salary) {
+    public Employee(String name, String surname, String department, int salary) {
         this.name = name;
         this.surname = surname;
         this.department = department;
@@ -64,11 +64,11 @@ public class Employee {
         this.department = department;
     }
 
-    public String getSalary() {
+    public int getSalary() {
         return salary;
     }
 
-    public void setSalary(String salary) {
+    public void setSalary(int salary) {
         this.salary = salary;
     }
 }
